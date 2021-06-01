db9f53eee00cc5bbd6c4d2c24f1f2eb5af0ad6fc
Radhakanta Roy <radhakantaroy22@gmail.com>
2021-06-01 21:36:22 +0530

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+var uiController = (function(){
+
+    var allSelectors, sliderxContainer, sliderxImgConiner, sliderxImg, slidexNavNext, sliderxNavPrev, sliderxIndicator;
+
+    allSelectors = {
+        sliderxCont: '.sliderx__container',
+        imgCont: '.sliderx__img-container',
+        sliderNavCont: '.sliderx__nav',
+        sliderIndicCont: '.sliderx__indicator'
+    };
+})();
+
+var globalController = (function(uiCtrl){
+    
+})(uiController);
